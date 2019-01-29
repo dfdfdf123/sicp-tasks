330feed7eacbaad853643e62fd1739c7698239d3
johnromanoff <johnromanoff@yandex.ru>
2019-01-29 23:38:00 +0300

    update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject sicp-tasks "0.1.0-SNAPSHOT"
   :description "sicp tasks"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/johnromanoff/sicp-tasks"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
